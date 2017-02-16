4b42d461e28e3845faaf23d3aab832d72fcc6717
uzimith <uzimith.x9@gmail.com>
2017-02-17 00:26:16 +0900

    show hoge: hogehoge

---
+++ app.js
@@ -0,0 +1,21 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400,
+        resizable: false
+        });
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});

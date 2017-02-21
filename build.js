c4d1e68368953ac96440741058f2b2bea7153947
uzimith <uzimith.x9@gmail.com>
2017-02-21 10:43:11 +0900

    add watch

--- build.js
+++ build.js
@@ -1,9 +1,14 @@
 var packager = require('electron-packager');
 packager({
     dir: 'app',
+    out: './dist',
     buildPath: 'twitchComments',
     electronVersion: '1.4.15',
     platform : 'darwin',
+    // platform : 'win32',
     arch: 'x64',
     overwrite: true
-}, function() {});
+}, function(err, appPaths) {
+    if (err) console.log(err);
+    console.log('Done: ' + appPaths);
+});
---
+++ watch.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const webpack = require('webpack')
+const conf = require('./webpack.config.js')
+
+const compiler = webpack(conf)
+
+let electron = null
+
+compiler.watch({}, (err, stats) => {
+    if (err) {
+        console.dir(err)
+        return
+    }
+
+    if (stats.hasWarnings()) {
+        stats.compilation.warnings.forEach(warning => {
+            console.dir(warning)
+        })
+    }
+
+    if (stats.hasErrors()) {
+        stats.compilation.errors.forEach(error => {
+            console.log(error.error.toString())
+            if (error.error.codeFrame) {
+                console.log(error.error.codeFrame)
+            }
+        })
+        return
+    }
+
+    if (!electron) {
+        electron = require('electron-connect').server.create({ path: 'app' })
+        electron.start()
+        electron.on('quit', () => process.exit(0))
+    } else {
+        electron.restart()
+    }
+})
